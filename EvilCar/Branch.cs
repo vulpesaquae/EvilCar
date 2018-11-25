07f8e4d10f72049acc03c17feb30b0b487cefc54
SeiferthF <SeiferthF@NB2737>
2018-11-25 14:16:52 +0100

    Added Branch and Fleet classes; Empty;

---
+++ EvilCar/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilCar
+{
+    // Nur wegen vollständigkeit hier
+    // Keine Ahnung was das sein soll.
+    // Achim und ich hatten uns erstmal drauf geeinigt, dass das wie eine Flotte ist...
+    class Branch
+    {
+    }
+}

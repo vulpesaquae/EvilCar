002a24f704de81591c52c62aa8113600eaacd231
SeiferthF <SeiferthF@NB2737>
2018-11-25 14:21:43 +0100

    Added Comment in Program and Car;

--- EvilCar/Car.cs
+++ EvilCar/Car.cs
@@ -8,6 +8,16 @@ namespace EvilCar
 {
     class Car
     {
+        // 3
+        // Services können wir auch als eigene Klasse machen
+        // Wäre nur die Frage, ob wir pro Service eine Klasse erstellen oder eine mit allen
+        // Dadurch bräuchten wir hier nur ein Dictionary mit gebuchten Services
+        // Die Preise sind dann ja in den Klassen drin
+        // Müssten halt schauen wie wir das mit der individuellen zuordnung machen
+        // Massage darf ja nur für "Big Limousine" verfügbar sein
+        // Mein erster Gedanke wäre, dass das dann wieder auf hardcoden hinausläuft
+
+        // 2
         // Ist wohl besser die Autokonfigurationen in einer Datei zu speichern und dann auszulesen
         // Dadurch ist man flexibler was Änderungen angeht und es können auch manuell neu hinzugefügt werden
         // Ist dann auch einfacher individuell was für Automarken zu machen...
@@ -15,6 +25,7 @@ namespace EvilCar
         // Alles andere wird dann hardcoded...also kosten, Services und Preise...
         public float Costs;
 
+        // 1
         // Bin mir nicht sicher, ob das hier der richtige Platz für ist.
         // Müssen das pro "Autoart" individuell anpassen können, also wohl eher nicht. (S. 11, Projektbeschreibung, letzter Satz)
         // Bin aber gerade zu faul mir deswegen groß Gedanken zu machen...vielleicht später nocht...
--- EvilCar/Program.cs
+++ EvilCar/Program.cs
@@ -8,6 +8,8 @@ namespace EvilCar
 {
     class Program
     {
+        // Laut Domain Model in der Projektbeschreibung (S. 6) hätte ich mir Administrator und FleetManager Klasse sparen können.
+        // Müssten da eh schauen, wie wir das mit der Authetifizierung machen wollen.
         static void Main(string[] args)
         {
             Console.WriteLine("nice");

4a608e98867c68cd8ca0115d89c381d39795aee3
SeiferthF <SeiferthF@NB2737>
2018-11-25 13:56:47 +0100

    Added FleetManager class; Functions are not implemented;

---
+++ EvilCar/FleetManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilCar
+{
+    class FleetManager
+    {
+        public bool Customer_Create(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Customer_Update(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Customer_Read(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        // able to administer his fleet (CRUD)
+        // Keine Ahnung was er meint...
+        public bool Fleet_Crud()
+        {
+            throw new NotImplementedException();
+        }
+
+        // able to administer his cars (CRUD)
+        // Keine Ahnung was er meint...
+        public bool Cars_Crud()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalculateCosts(string car, List<string> bookedServices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
